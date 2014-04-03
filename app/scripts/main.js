f4cbeb151ffd9d2d84659908f788f049d01c3eeb
Jake Tracey <jaketracey@gmail.com>
2014-04-03 20:59:13 +1100

    intro and kitchen

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,29 @@
-console.log('\'Allo \'Allo!');
+$( document ).ready(function() {
+    // Init
+    $('.goToKitchen').click(function() {
+        $('#homeMenu').addClass('hinge');
+
+        $('#KitchenContainer').addClass('animated fadeIn');
+        $('#container').addClass('animated fadeOut');
+
+        $('#kitchenInfo').addClass('animated bounceInUp');
+        $('#container').css( "zIndex", "5" );
+        $('#KitchenContainer').css( "zIndex", "10" );
+
+
+    });
+
+    $('#kitchenSubMenu #menuHome').click(function() {
+        $("#menuHome").addClass('animated rubberBand');
+        $("#KitchenContainer").removeClass('fadeIn');
+        $("#KitchenContainer").addClass('fadeOut');
+
+        $("#container").show();
+        $("#homeMenu").removeClass("hinge");
+        $("#container").removeClass('fadeOut');
+        $('#container').css( "zIndex", "10" );
+        $('#KitchenContainer').css( "zIndex", "5" );
+
+
+    })
+});
